d5b6d8dab948bcc655d5d9e4e27d9ce9ccddbeee
Nguyen Duc <99275934+tranghamham@users.noreply.github.com>
2022-08-13 22:36:31 +0700

    Init Sample Data

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Column from '../Column/Column'
+import { isEmpty } from 'lodash'
 import './BoardContent.scss'
+import { initialData } from '../../actions/initialData'
+import { mapOrder } from '../../utilities/sorts'
 
 function BoardContent() {
+    const [board, setBoard] = useState({})
+    const [columns, setColumns] = useState([])
+
+    useEffect(() => {
+      const boardFromDB = initialData.boards.find(board => board.id === 'board-1')
+      if(boardFromDB) {
+        setBoard(boardFromDB)
+
+        // Sort Advanced
+        // boardFromDB.columns.sort(function(a, b){
+        //   return boardFromDB.columnOrder.indexOf(a.id) - boardFromDB.columnOrder.indexOf(b.id)
+        // })
+
+        setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, 'id'))
+      }
+    }, [])
+
+    if(isEmpty(board)) {
+      return <div className='not-found' style={{ padding: '10px', color: '#fff' }}>Board not found!</div>
+    }
+
     return (
         <div className='board-content'>
-          <Column />
-          <Column />
-          <Column />
-          <Column />
-          <Column />
+          {columns.map((column, index) => <Column key={index} column={column} />)}
         </div>
     )
 }
